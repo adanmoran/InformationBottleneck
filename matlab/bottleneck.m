be62d2bfb222737b8ef9acb53d95a429e5f0f927
Adan Moran-MacDonald <adanmoranm@gmail.com>
2018-07-11 14:26:57 -0400

    Update the bottleneck function
    The updates are the following:
    * Outputs are the same as in the ib() function, plus H(T) and H(T|X)
    * The case where beta = Inf is now handled.
    * The iteration algorithm now runs for all parameters of alpha, beta,
    and gamma.
    * The updateQtgx function successfully computes the distribution for all
    alpha, gamma, and beta
    * After computing q(t), we remove any T-values which have zero
    probability from both q(t) and q(t|x). This stops q(y|t) from having NaN
    values when we divide by q(t).

--- matlab/bottleneck.m
+++ matlab/bottleneck.m
@@ -28,7 +28,8 @@
 % * Qtgx = Conditional distribution of T given X
 % * Ixt = Mutual Information I(X;T)
 % * Iyt = Mutual Information I(T;Y)
-% * H = Renyi Entropy H_gamma(T)
+% * Ht = Renyi Entropy H_gamma(T)
+% * Htgx = Conditional Entropy H(T|X)
 %
 % If gamma = 1, we have two special cases: 
 % * alpha = 1 -> Tishby's Information Bottleneck (1999)
@@ -36,12 +37,21 @@
 %
 % If gamma is non-zero, then we have the results obtained by
 % Moran-MacDonald et.al. (2018)
-function [Qtgx, Ixt, Iyt, H] = bottleneck(Pxy, beta, alpha, gamma, epsilon)
-    % Set defaults for 5th parameter and validate 4th parameter
+function [Qtgx, Qt, L, Ixt, Iyt, Ht, Htgx] = bottleneck(Pxy, ...
+                                                        beta, ...
+                                                        alpha, ...
+                                                        gamma, ...
+                                                        epsilon, ...
+                                                        debug)
+    % Set defaults for 6th parameter
+    if nargin < 6
+        debug = false;
+    end
+    % Set defaults for 5th parameter
     if nargin < 5
         epsilon = 10^-8;
     end
-    % Set defaults for 4th parameter and validate 3rd parameter
+    % Set defaults for 4th parameter
     if nargin < 4
         gamma = 1;
     end
@@ -61,24 +71,51 @@ function [Qtgx, Ixt, Iyt, H] = bottleneck(Pxy, beta, alpha, gamma, epsilon)
     % If beta is zero, all X values are compressed to one point
     if beta == 0
         % Assign a conditional probability of 1 to T = 1
-        Qtgx = zeros(n);
+        Qtgx = zeros(size(Pxy,1));
         Qtgx(:,1) = 1;
         % Mutual information of a deterministic function is 0
         Ixt = 0;
         Iyt = 0;
         % Renyi entropy of a deterministic function is 0
-        H = 0;
+        Ht = 0;
+        Htgx = 0;
+        % L-functional value is 0 - beta*0 = 0
+        L = 0;
         return;
     end
-    
+        
     % Get the distribution of X and throw away any small decimals to avoid
     % floating point errors
     Px = makeDistribution(sum(Pxy,2));
     % |T| is always |X|
     n = length(Px);
     
-    % Randomly initialize q(t|x), which is of size |X| x |T|
-    Qtgx = makeDistribution(randi(10000, n, n),2);
+    % Get the distribution of Y given X and ensure the row sum is
+    % identically 1.
+    Pygx = makeDistribution(Pxy ./ Px, 2);
+    
+    % If beta is infinity, we must return T as equal to X
+    if beta == Inf
+        % Probability of T given X is an identity
+        Qtgx = eye(n);
+        % Mutual information I(X;T) = I(X;X) = H(X)
+        Ixt = entropy(Px);
+        % Mutual information I(T;Y) = I(X;T)
+        Iyt = mi(Pygx,Px);
+        % Renyi entropy is H_gamma(X)
+        Ht = entropy(Px, gamma);
+        % Conditional entropy H(X|X) = 0.
+        Htgx = 0;
+        % L is clearly negative infinity
+        L = -Inf;
+        return;
+    end
+    
+    % Randomly initialize q(t|x), which is of size |X| x |T|, with values
+    % that have decimal places as small as 10^-5. This is small, but not so
+    % small that we cannot have a distribution that sums to identically 1
+    % due to rounding errors.
+    Qtgx = makeDistribution(randi(100000, n, n), 2);
     
     % Initialize q(t) based on q(t|x)
     Qt = updateQt(Qtgx,Px);
@@ -88,6 +125,61 @@ function [Qtgx, Ixt, Iyt, H] = bottleneck(Pxy, beta, alpha, gamma, epsilon)
     
     % TODO: Code the generalized information bottleneck functional
     
+    % Run the iteration algorithm until convergence.
+    converged = false;
+    F = Inf;
+    while ~converged
+        % Update the distribution of T given X
+        [newQtgx,Z] = updateQtgx(Qygt, Qt, Pygx, beta, alpha, gamma);
+        newQt = updateQt(newQtgx,Px);
+        % If q(t) is zero anywhere, discard those rows in q(t) and columns
+        % in q(t|x) before updating q(y|t).
+        nonZeroT = newQt > 0;
+        newQt = newQt(nonZeroT);
+        newQtgx = newQtgx(:, nonZeroT);
+        % Update q(y|t) using these non-zero values.
+        newQygt = updateQygt(newQtgx,newQt,Pxy);
+        % Update the functional which minimizes the distributions, which is
+        % given by Ex[log(Z(x,beta))]
+        newF = -dot(Px,log(Z));
+        if debug
+        fprintf('F is %.8f, newF is %.8f, diff is %.9f\n',...
+                F, newF, (F - newF));
+        end
+            
+        % Compute the joint distribution of T and X for comparison
+        newQtx = makeDistribution(newQtgx.*Px);
+        oldQtx = makeDistribution(Qtgx.*Px);
+        
+        % If we removed elements from q(t|x), this cannot have converged.
+        % Thus, we only check for convergence if the new and old
+        % distributions are the same size.
+        if size(newQtx) == size(oldQtx)
+            % The distribution has converged if the improvement is minimal
+            JS = jsdiv(newQtx, oldQtx, 0.5, 0.5);
+            if JS < epsilon
+                converged = true;
+            end
+        end
+        
+        % Update the distributions
+        F = newF;
+        Qtgx = newQtgx;
+        Qt = newQt;
+        Qygt = newQygt;  
+    end
+    % Compute the entropy H_gamma(T)
+    Ht = entropy(Qt, gamma);
+    % TODO: Compute the conditional entropy H(T|X)
+    Htgx = condentropy(Qtgx,Px);
+    % Compute the mutual information I(X;T) and I(Y;T) for the IB plane
+    Ixt = mi(Qtgx,Px);
+    Iyt = mi(Qygt,Qt);
+    % Compute L
+    L = Ht - alpha*Htgx - beta*Iyt;
+    if debug
+        fprintf('----- Converged with L = %.8f -----\n', L);
+    end
 end
 
 function Qt = updateQt(Qtgx,Px)
@@ -104,4 +196,75 @@ function Qygt = updateQygt(Qtgx,Qt,Pxy)
     QygtQt = Qtgx' * Pxy;
     % Normalize along the rows to ensure the row sum is 1.
     Qygt = makeDistribution(QygtQt ./ Qt, 2);
+end
+
+function [Qtgx, Z] = updateQtgx(Qygt, Qt, Pygx, beta, alpha, gamma)
+    % Compute the KL divergence between p(y|x) and q(y|t), which goes in
+    % the exponential factor of the new q(t|x).
+    betaDivergence = beta .* divmat(Pygx,Qygt);
+    
+    % Compute the factor in the exponential which relates to q(t),
+    % depending on which gamma was chosen
+    if gamma == 1
+        % In the standard IB/DIB, the q(t) term in the exponential
+        % is just the negative logarithm.
+        qtTerm = -log(Qt);
+    else % gamma is not 1
+        % The gamma factor comes from the derivative of Renyi entropy.
+        gammaFactor = gamma / (1 - gamma);
+        % q(t) taken to the power of gamma also comes from the derivative 
+        % of Renyi entropy.
+        qtGammaFactor = 1 / sum(Qt .^ gamma);
+        % The actual term in the exponential involves q(t) taken to the
+        % gamma.
+        qtTerm = (gammaFactor * qtGammaFactor) .* (Qt.^(gamma - 1));
+    end
+    
+    % In the case where alpha is 1 and gamma is 1, we get the standard IB,
+    % so the log(q(t)) comes out of the exponential
+    % q(t|x) = q(t)*exp{-beta Dkl[p(y|x) || q(y|t)]} / Z
+    if alpha == 1 && gamma == 1
+        % Since Qt is a column vector of size |T| and the exponential is a 
+        % matrix of size |X| x |T|, we need to transpose Qt to get it to 
+        % multiply across the columns of the exponential matrix.
+        Qtgx = Qt' .* exp(-betaDivergence);
+        
+    % In the case where alpha is non-zero, we get the typical exponential
+    % q(t|x) = exp{-1/alpha*( qtTerm + beta*Dkl[p(y|x) || q(y|t)])} / Z
+    elseif alpha ~= 0
+        % Since Qt is a column vector of size |T| and betaDivergence is a
+        % matrix of size |X| x |T|, we need to transpose Qt to get it to
+        % add element-by-element along the columns of betaDivergence.
+        Qtgx = exp(-1/alpha .* (qtTerm' + betaDivergence));
+        
+    % If alpha == 0, we get the DIB or Renyi-DIB
+    % q(t|x) = delta(t - argmin_t{ qtTerm + beta*Dkl[p(y|x) || q(y|t)] }
+    else
+        % Since Qt is a column vector of size |T| and betaDivergence is a
+        % matrix of size |X| x |T|, we need to transpose Qt to get it to
+        % add element-by-element along the columns of betaDivergence.
+        % We also have to take the minimum along each row, since that's the
+        % minimum T for each fixed X. The result is a column vector with
+        % locations of the minimum T for each X.
+        [~,argmins] = min(qtTerm' + betaDivergence, [], 2);
+        
+        % Get the size of |X|
+        n = size(betaDivergence,1);
+        % Get the size of |T|
+        k = length(Qt);
+        
+        % The q(t|x) term is a delta function, so we fill in the ones by
+        % using a sparse matrix representation and then getting the full
+        % representation of it. The argmins column vector shows column
+        % locations for the ones, while the rows go from 1 to k in order.
+        rows = 1:n;
+        Qtgx = full(sparse(rows,argmins,1,n,k));
+    end
+    
+    % Get the normalization factor as an output variable, which is the
+    % column vector of sums along the fixed rows.
+    Z = sum(Qtgx, 2);
+    % Normalize the distribution along the rows and enforce the row sum as
+    % identically 1.
+    Qtgx = makeDistribution(Qtgx ./ Z, 2);
 end
