2e401aa37f012cf412c356ded84e795b8148441c
Adan Moran-MacDonald <adanmoranm@gmail.com>
2018-07-16 15:55:39 -0400

    Starting to compute curve once betas are known
    Added a waitbar to the beta finder.
    
    Created a function which will get the curve points H(T), H_gamma(T),
    I(X;T) and I(Y;T) for given betas. This is to be run after the betas
    have been computed, so as to compute the positions on the curve for all
    of these.
    
    The curve will minimize the L functional for the generalized bottleneck
    functional, but the function will return the positions on all planes for
    all the inputted betas. These can then be plotted.

--- matlab/bottlecurve.m
+++ matlab/bottlecurve.m
@@ -109,13 +109,19 @@ function [Ixt,Ht,Hgt,Iyt,Bs] = bottlecurve( Pxy,...
     Ixy = mi(Pygx,Px);
     
     % If betas are not given, find the values of these betas which will fit
-    % the desired partition
+    % the desired partition in the H_gamma(T) - alpha*H(T|X) axis.
     if isempty(betas)
-        Bs = findBetaPartition(Pxy, N, Hgx, alpha, gamma, delta, epsilon);
+        Bs = findBetaPartition(Pxy, N, Hgx, alpha, gamma, delta, ...
+                               epsilon);
     else
         Bs = betas;
     end
     %% TODO: Compute the curve for the betas found and handle all input conditions for display.
+    % Compute the positions on the curve for each of the planes, where
+    % Hs is entropies H(T), Hgas is the generalized entropy
+    % H_gamma(T) - alpha H(T|X), IbXs is the mutual information I(X;T),
+    % and IbYs is the mutual information I(T;Y).
+    [Hs,Hgas,IbXs,IbYs] = getCurvePoints(Pxy,Bs,alpha,gamma,epsilon);
     
     % Temporarily set outputs so the function works in testing. 
     % TODO: Remove these as they will be recomputed later.
@@ -125,6 +131,8 @@ function [Ixt,Ht,Hgt,Iyt,Bs] = bottlecurve( Pxy,...
     Iyt = 0;
 end
 
+%% Validation
+% Validate all inputs to the public function, bottlecurve().
 function validate(N, alpha, gamma, delta, epsilon, display, betas)
     % Ensure all numerical inputs are valid.
     assert(N > 0, "BottleCurve: N must be positive.");
@@ -191,6 +199,8 @@ end
 % which meet the requirements of splitting the Hga(T,X) plane into N
 % regions.
 function betas = findBetaPartition(Pxy,N,Hgx,alpha,gamma,delta,epsilon)
+%% TODO: Update this function to only display printouts if debug mode is on.
+%% TODO: Add a waitbar to this function so users can get feedback (or have the waitbar passed in as a parameter).
     % Distance between two partition values of H_gamma(X)
     partitionDist = Hgx / N;
     % Partition goes from 0 to H_gamma(X)
@@ -205,6 +215,11 @@ function betas = findBetaPartition(Pxy,N,Hgx,alpha,gamma,delta,epsilon)
     % Set how much we will increment the right boundary of a bisection
     % method when initializing the search space for betas.
     rightBetaIncrement = 10;
+    
+    % Initialize a waitbar so we can display the results of the search.
+    bar = waitbar(0,'', 'Name', 'Find Beta Partition');
+    % Format string for the waitbar
+    fmt = 'Beta search %d of %d (Hga = %.3f):\n %s';
 
     % Search through the partition to find the values we desire. The
     % partition goes from 0 to H_gamma(X), but the value of 0
@@ -212,7 +227,7 @@ function betas = findBetaPartition(Pxy,N,Hgx,alpha,gamma,delta,epsilon)
     % to beta = Inf. Thus, we only need to search for betas within
     % [1/N*H(X), ... ,(N-1)/N*H(X)]. This corresponds to indices
     % 2:(N-1)
-    for i = 2:(N-1)
+    for i = 2:(N-1) 
         % The horizontal axis value we are searching for is given by
         % the partition.
         HgaToFind = partition(i);
@@ -229,6 +244,10 @@ function betas = findBetaPartition(Pxy,N,Hgx,alpha,gamma,delta,epsilon)
         rightBeta = ceil(betas(i-1)) + rightBetaIncrement;
         rightmostBetaFound = false;
 
+        % Update the waitbar
+        waitbar((i-1)/N, bar, sprintf(fmt, (i-1), N, HgaToFind, ...
+            'Finding Beta Boundaries.'));
+        
         % Keep increasing the rightmost beta value until we find one
         % where Hga > HgaToFind, so that we can perform the bisection
         % method in the next loop.
@@ -254,7 +273,7 @@ function betas = findBetaPartition(Pxy,N,Hgx,alpha,gamma,delta,epsilon)
                 rightmostBetaFound = true;
             end
         end
-
+        
         % Traverse through beta values using a binary search to find
         % the betas which result in a bottleneck value that has 
         % Hga = HgaToFind
@@ -269,6 +288,11 @@ function betas = findBetaPartition(Pxy,N,Hgx,alpha,gamma,delta,epsilon)
             [~,~,~,~,~,Hgt,Htgx] = bottleneck(Pxy, currentBeta, ...
                                               alpha, gamma, epsilon);
             Hga = Hgt - alpha * Htgx;
+            
+            % Update the waitbar
+            waitbar((i-1)/N, bar, strcat(...
+                sprintf(fmt, (i-1), N, HgaToFind,'Searching for Beta '), ...
+                sprintf(' (Hga = %.4f)', Hga)));
             fprintf('Beta %d gave Hga = %.9f\n',currentBeta,Hga);
             % Compute the difference between the current value and
             % where we want to be.
@@ -308,4 +332,70 @@ function betas = findBetaPartition(Pxy,N,Hgx,alpha,gamma,delta,epsilon)
             end
         end
     end
+    
+    % Clear the waitbar
+    delete(bar);
+end
+
+%% Find Generalized Curve Points
+% Given some beta values and parameters for the bottleneck, this will
+% compute the entropy H(T), generalized renyi-alpha information
+% H_gamma(T) - alpha*H(T|X), and mutual informations I(X;T) and I(T;Y) for
+% all the beta values.
+%
+% This is done by iterating the bottleneck functional multiple times and
+% taking distribution q(t|x) which minimizes the L-functional
+% L = H_gamma(T) - alpha*H(T|X) - beta*I(T;Y)
+%
+% Every time a lower L value is found, a loop cycle resets. If no lower L
+% value is found within a maximum number of iterations, the loop cycle ends
+% and the bottleneck points associated with that lowest L value are added
+% to the output lists.
+%
+% Inputs:
+% * Pxy = a joint distribution of X and Y
+% * Bs = beta values to optimize
+% * alpha = parameter in front of H(T|X) in the L-functional
+% * gamma = renyi parameter for the entropy of T
+% * epsilon = convergence parameter for the bottleneck function
+% * maxIterations = number of iterations 
+%
+% Outputs:
+% * Hs = entropy values for T, H(T)
+% * Hgas = generalized entropy values, H_gamma(T) - alpha*H(T|X)
+% * IbXs = mutual information I(X;T)
+% * IbYs = mutual information I(T;Y)
+function [Hs,Hgas,IbXs,IbYs] = getCurvePoints(Pxy, Bs, alpha, ...
+                                              gamma,epsilon,maxIterations)
+    % Number of elements in the beta list, used for initializing output
+    % vectors to speed up allocation
+    numBetas = length(Bs);
+    
+    % Initialize the output vectors
+    Hs = zeros(1,numBetas);
+    Hgas = zeros(1,numBetas);
+    IbXs = zeros(1,numBetas);
+    IbYs = zeros(1,numBetas);
+    
+    % Initialize a waitbar to display output to the user
+    bar = waitbar(0,'','Name','Compute Curve Points');
+    % Counter to keep track of how many iterations have gone, for the
+    % waitbar to update properly. Initialize to zero since we add 1 to it
+    % immediately in the loop.
+    betaIndex = 0;
+    
+    % Loop over each beta and compute the output values for each one
+    for beta = Bs
+        % Update the index counter for the waitbar
+       betaIndex = betaIndex + 1;
+       % Update the waitbar itself
+       waitbar(betaIndex / numBetas, bar, ...
+           sprintf('Computing bottleneck for beta = %.2f\n (%d of %d)',...
+                beta, betaIndex, numBetas));
+    % TODO: Perform a search for the optimal L for this beta given the
+    % inputs.
+    end
+    
+    % Close the waitbar
+    delete(bar);
 end
