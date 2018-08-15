bb801be339412155187e6018e7dcf366d53802b2
Adan Moran-MacDonald <adanmoranm@gmail.com>
2018-08-15 11:15:04 -0400

    Finished the bottlecluster function
    This function computes the bottleneck curve for a set of points using
    the results from Strouse and Schwab's "Geometric Clustering" paper. This
    can compute the clusters for hardcoded betas - once the DIB can search
    for the beta partitions, this will work much better.
    
    It also returns a struct containing all the desired data, such as the
    DIB curve, the beta values, the points, etc... so that we can analyze
    the data later

--- matlab/bottlecluster.m
+++ matlab/bottlecluster.m
@@ -11,11 +11,34 @@
 % * M (optional) = number of grid points per coordinate of R^n, which is
 % used to compute the number of points in p(i,x). This matrix will be of
 % size N x M^n and is fed into the (Renyi-)DIB
+% * gamma (optional) = Renyi parameter for the bottleneck curve. Must be in
+% ]0,Inf[. Default is 1.
+% * beta (optional) = beta value which we use to compute the cluster after
+% the DIB is computed. This is TEMPORARY as we should be able to
+% automatically search for the partition and optimal beta (where there is
+% highest theta) later.
 %
 % Outputs:
 % * Qcgi = A conditional distribution which determines which cluster c
 % contains point i.
-function Qcgi = bottlecluster(points,s,M)
+% * QStruct = A struct containing the following:
+%    - The Renyi-DIB X-axis, H_g(T)
+%    - The Renyi-DIB Y-axis, I(Y;T)
+%    - The beta values used to compute each of these points
+%    - The gamma value chosen
+%    - The points themselves
+%    - The distribution of the grid P(x), as a gmdistribution
+%    - The pdf of P(x) (denoted f(x)) at the specified grid points
+%    - The joint distribution p(i,x) used to compute the Renyi-DIB
+%    - The grid points which generated the x in p(i,x) and f(x)
+%    - The cluster distribution q(c|i) for the beta value inputted (or the
+%    kink beta, if that was chosen)
+%    - The cluster indices c, as a vector of cluster numbers
+%    - The probability of each cluster number appearing, q(c), as a vector
+%    - The probability of each point on the grid, q(c|x)
+%    - The inputted beta value which was chosen for q(c|i)
+%    - The beta which gives the kink, if beta was not an input
+function QStruct = bottlecluster(points,s,M,gamma,beta)
     % Set default for second parameter
     if nargin < 2
         s = 1;
@@ -24,6 +47,14 @@ function Qcgi = bottlecluster(points,s,M)
     if nargin < 3
         M = 100;
     end
+    % Set defaults for the 4th parameter.
+    if nargin < 4
+        gamma = 1;
+    end
+    % Validate all parameters.
+    assert(s > 0, 'Cluster: s must be positive.');
+    assert(M > 0, 'Cluster: M must be positive.');
+    assert(gamma > 0 && gamma < Inf, 'Cluster: gamma must be in ]0,Inf[');
     
     % Get the number of points
     N = size(points,1);
@@ -38,6 +69,7 @@ function Qcgi = bottlecluster(points,s,M)
     grid = cell(n,1);
     % Find the minimum value of the points along each axis. Transpose to
     % make it a column vector. Subtract 2 to extend the grid slightly.
+    %% TODO: Instead of adding or subtracting 2, we should find the spot where p(x) < epsilon for some epsilon
     minima = transpose(min(points, [], 1)) - 2;
     % Now find the maximum value of these points along each axis. Transpose
     % to make it a column vector. Add 5 to extend the grid slightly.
@@ -151,5 +183,88 @@ function Qcgi = bottlecluster(points,s,M)
     title('Heatmap for p(i,x)');
     xlabel('Data point location, x');
     ylabel('Data point index, i');
+    
+    % Set the parameters for the bottleneck curve. Some of the values go
+    % unused when we choose betas, so we assign arbitrary values to those.
+    % Alpha must be zero for this as we are using (Renyi-)DIB clustering.
+    % We want to see all the plots.
+    num = 1; % unused
+    alpha = 0;
+    delta = 0.1; % unused
+    epsilon = 10^-8;
+    display = 'all'; % Show all plots
+    % We manually choose betas that are going to be used for this curve,
+    % since the bottlecurve partition function does not work on the DIB
+    % yet. Once that is fixed, we can opt to have the bottlecurve compute
+    % them for us.
+    betas = cat(2, 0:0.5:10, 10:20, 20:5:100,100:10:200, 200:25:500, Inf);
+    
+    % Now compute the bottleneck curve for this Pix
+    [Hga,~,~,Iyt] = bottlecurve(Pix, num, alpha, gamma, ...
+                                   delta, epsilon, display, betas);
+                               
+    % Get all the unique components of the curve, as there are often
+    % doubles
+    [uniqueHga, uniqueHgaIndex] = unique(Hga);
+    uniqueIyt = Iyt(uniqueHgaIndex);
+    % Now display this "filtered" curve
+    figure;
+    plot(uniqueHga, uniqueIyt);
+    
+    % Request input (for now): which beta should we choose? Display the
+    % combinations for all to see.
+    cat(1,uniqueHga,betas(uniqueHgaIndex))
+    if nargin < 5
+        beta = input('Which beta is the kink angle? ');
+    end
+    
+    % Compute the cluster for this beta
+    [Qcgi, Qc] = optimalbottle(Pix,gamma,alpha,beta);
+    
+    % Remove the clusters that have zero probability
+    Qcgi = Qcgi(:,Qc ~= 0);
+    Qc = Qc(Qc ~= 0);
+    
+    % Now multiply the matrix by the c-values to get the cluster locations
+    % for each i
+    clusterLabels = transpose(1:size(Qcgi,2));
+    c = Qcgi * clusterLabels;
+    
+    % Display the scatter plot, the contour, and the clusters if we can.
+    if n == 2
+        figure;
+        % Show the points
+        gscatter(points(:,1),points(:,2),c)
+        hold on
+        % Show the contour plot
+        contour(grid{1},grid{2},z);
+        title(sprintf('Contour plot, s = %d',s));
+        xlabel('X-coordinate');
+        ylabel('Y-coordinate');
+        hold off;
+    end
+
+    % Find the distribution q(c|x) = sum_i P(i,x)q(c|i) / P(x)
+    QcgxPx = transpose(Pix) * Qcgi;
+    Qcgx = makeDistribution(QcgxPx ./ fx, 2);  
+    
+    % Set all the outputs in a struct for easy access later
+    QStruct.Hga = Hga;
+    QStruct.Iyt = Iyt;
+    QStruct.betas = betas;
+    QStruct.gamma = gamma;
+    QStruct.points = points;
+    QStruct.Px = Px;
+    QStruct.fx = fx;
+    QStruct.Pix = Pix;
+    QStruct.grid = grid;
+    QStruct.Qcgi = Qcgi;
+    QStruct.c = c;
+    QStruct.Qc = Qc;
+    QStruct.Qcgx = Qcgx;
+    QStruct.inputBeta = beta;
+    if nargin < 5
+        QStruct.kinkBeta = beta;
+    end
 end
 
